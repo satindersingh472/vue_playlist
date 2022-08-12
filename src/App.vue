ed2548d89eb815b5da0de2c244e99da2afee4ce7
satinder <satindersingh472@gmail.com>
2022-08-11 20:23:13 -0600

    started

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+  <song-display></song-display>
+  </div>
+</template>
+
+<script>
+import SongDisplay from '@/components/songDisplay.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    SongDisplay
+
+  }
+}
+</script>
+
+<style>
+
+</style>
