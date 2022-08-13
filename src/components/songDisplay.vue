152b11c2612411c6a16dc4e0a1101e4426906c7e
satinder <satindersingh472@gmail.com>
2022-08-13 08:39:18 -0600

    finished

--- src/components/songDisplay.vue
+++ src/components/songDisplay.vue
@@ -1,49 +1,47 @@
 <template>
-<!-- all is the parent div -->
+  <!-- all is the parent div -->
   <div class="all">
     <!-- h1 is the heading -->
     <h1>List of Songs</h1>
-<!-- list div will have a list  of songs -->
+    <!-- list div will have a list  of songs -->
     <div class="list" ref="all_songs">
-        <!-- list item is every songs that is in the list -->
-      <div
-        class="list_item"
-        v-for="song in songs"
-        :key="song[`id`]"
-     
-      >
-      <!-- items div will contain all the information about song in the list and if we click on the div there is an event calling current song function -->
+      <!-- list item is every songs that is in the list -->
+      <div class="list_item" v-for="song in songs" :key="song[`id`]">
+        <!-- items div will contain all the information about song in the list and if we click on the div there is an event calling current song function -->
         <div class="items">
           <button @click="current_song">Song: {{ song[`title`] }}</button>
           <h2>Singer: {{ song[`artist`] }}</h2>
           <p>Song_Id: {{ song[`song_id`] }}</p>
-          <img :src="song[`image_url`]" :alt="song[`song_id`]" />
+          <img :src="song[`image_url`]" :alt="`image of ${song[`song_id`]}`" />
         </div>
       </div>
     </div>
     <!-- display playing div will contain the current song -->
-      <div class="display_playing">
-        <h2>playing song</h2>
-        <div ref="song_playing">
-        <h2>your selected song will get displayed here<br>just press the song button with name</h2>
-        </div>
+    <div class="display_playing">
+      <h2>playing song</h2>
+      <div ref="song_playing">
+        <h2>
+          your selected song will get displayed here<br>just press the song
+          button with name
+        </h2>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    // methods will contain functions
+  // methods will contain functions
   methods: {
-    // current song will get details[`outerHTML] of clicked items and show them inside ref song playing 
+    // current song will get details[`outerHTML] of clicked items and show them inside ref song playing
     current_song(details) {
-        this.$refs[`song_playing`][`innerHTML`] = details[`target`][`parentElement`][`outerHTML`];    
-        
-    }
+      this.$refs[`song_playing`][`innerHTML`] =
+        details[`target`][`parentElement`][`outerHTML`];
+    },
   },
   data() {
     return {
-        // songs is an array of objects containing list of various songs
+      // songs is an array of objects containing list of various songs
       songs: [
         {
           title: `peed`,
@@ -87,25 +85,25 @@ img {
   grid-template-columns: auto;
   justify-self: center;
 }
-.items{
-    font-size: 1rem;
+.items {
+  font-size: 1rem;
   display: grid;
   align-items: center;
   justify-items: start;
-   padding: 10px 10px 20px 20px;
+  padding: 10px 10px 20px 20px;
   box-shadow: 3px 3px 6px grey;
 }
-.all{
-    display: grid;
-    text-align: center;
+.all {
+  display: grid;
+  text-align: center;
 }
-h1{
-    order: 2;
+h1 {
+  order: 2;
 }
-.display_playing{
-    order: 1;
-    text-align: center;
-    display: grid;
-    place-items: center;
+.display_playing {
+  order: 1;
+  text-align: center;
+  display: grid;
+  place-items: center;
 }
 </style>
